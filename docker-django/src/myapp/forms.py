decffc8f61c9f95c62bb5ee1203b088a79dd2bcd
Jônathas David <66834877+jonathasDavid@users.noreply.github.com>
2025-08-04 17:17:43 -0300

    Delete docker-django directory

--- docker-django/src/myapp/forms.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from django.contrib.auth.models import User
-class FeedbackForm(forms.ModelForm):
-    user=forms.CharField(widget=forms.HiddenInput)
-    def clean_user(self):
-        user=User.objects.get(id=self._request.user.id)
-        return user
--- docker-django/src/myapp/serializers.py
+++
@@ -1,7 +0,0 @@
-from   .models import  Feedback
-from  rest_framework import  serializers
-
-class FeedbackSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Feedback
-        fields = '__all__'
